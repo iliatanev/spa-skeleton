2edf8952be5eafc85ef0adafc6068e29efbe3fc8
iliatanev <itanev@devrix.com>
2020-10-27 22:52:59 +0200

    small code refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const handlers = {};
 
 $(() => {
-    const app = Sammy( '#roots', function() {
+    const app = Sammy( '#root', function() {
         this.use( 'Handlebars', 'hbs' );
 
         this.get( '/index.html', handlers.getHome );
